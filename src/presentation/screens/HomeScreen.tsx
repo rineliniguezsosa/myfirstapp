19fd4490cab5dc140f65373c9dfc58cb529905a9
Rinel <rineliniguezsosa@gmail.com>
2024-09-27 14:16:48 -0600

    [FEAT] Multiples cambios en el archivo HomeScreen.tsx
    - importando StyleSheet
    - estableciendo las clases container y title
    - creando la variable styles donde creamos los estilos gloobales de la aplicación donde creamos la clase container y la clase title
    - css props en la clase container, flex:1 para ocupar todo el ancho y el alto, justifyContent:'center'
    - css props en la clase title, fontSize a 45, textAlign:'center', color: 'black' y padding de 20

--- src/presentation/screens/HomeScreen.tsx
+++ src/presentation/screens/HomeScreen.tsx
@@ -1,10 +1,23 @@
 import React from 'react';
-import { View,Text } from 'react-native';
+import { View,Text,StyleSheet } from 'react-native';
 
 export const HomeScreen = () => {
   return (
-    <View>
-        <Text>HomeScreen</Text>
+    <View style={styles.container}>
+        <Text style={styles.title}>HomeScreen</Text>
     </View>
-  )
-}
+  );
+};
+
+const styles = StyleSheet.create({
+    container:{
+        flex:1,
+        justifyContent:'center',
+    },
+    title:{
+        fontSize:45,
+        textAlign:'center',
+        color:'black',
+        padding:20,
+    },
+});
