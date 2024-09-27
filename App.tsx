4d59bd5b0ce3aa6f4708043931eb3053b7c45241
Rinel <rineliniguezsosa@gmail.com>
2024-09-27 15:16:31 -0600

    [FEAT] Retornando el componente CounterScreen

--- App.tsx
+++ App.tsx
@@ -1,11 +1,13 @@
+/* eslint-disable react-native/no-inline-styles */
 import React from 'react';
 import { SafeAreaView } from 'react-native';
-import { HomeScreen } from './src/presentation/screens/HomeScreen';
+// import { HomeScreen } from './src/presentation/screens/HomeScreen';
+import { CounterScreen } from './src/presentation/screens/CounterScreen';
 
 export const App = () => {
   return (
     <SafeAreaView style={{flex:1}}>
-      <HomeScreen name="Rinel iniguez"/>
+      <CounterScreen/>
     </SafeAreaView>
   );
 };
