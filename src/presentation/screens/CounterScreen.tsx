0e494fd5b9c3ff76006c998a5ef8c0d4402a3604
Rinel <rineliniguezsosa@gmail.com>
2024-09-27 14:36:24 -0600

    [FEAT] Multiples cambios en el archivo CounterScreen.tsx
    - importando el StyleSheet
    - estableciendo las clases container y title
    - creando la variable styles donde creamos los estilos globales
    - css props para la clase container, flex:1, justifyContent:'center', alignItems:'center'
    - css props para la clase title, fontSize:80, color:'black',fontWeight:800,

--- src/presentation/screens/CounterScreen.tsx
+++ src/presentation/screens/CounterScreen.tsx
@@ -1,10 +1,23 @@
 import React from 'react';
-import { View,Text } from 'react-native';
+import { View,Text,StyleSheet } from 'react-native';
 
 export const CounterScreen = () => {
   return (
-    <View>
-        <Text>CounterScreen</Text>
+    <View style={styles.container}> 
+        <Text style={styles.title}>CounterScreen</Text>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+    container:{
+        flex:1,
+        justifyContent:'center',
+        alignItems:'center',
+    },
+    title:{
+        fontSize:80,
+        color:'black',
+        fontWeight:800,
+    },
+});
