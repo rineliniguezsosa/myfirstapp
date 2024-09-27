5c1b841fb499642e445171b617cd17eb57b57935
Rinel <rineliniguezsosa@gmail.com>
2024-09-27 16:04:30 -0600

    [FEAT] Multiples cambios en el archivo CounterScreen.tsx
    - importando el componente PrimaryButton
    - eliminando la linea para deshabilitar eslint
    - eliminando las imoportaciones de Pressable y Platform
    - eliminando el método handleIncrement
    - estableciendo el componente PrimaryButton con las props label igual a Incrementar, prop onPress con la referencia de la función el cual incrementa el state en 1, onLongPress con la referencia de la función que actualiza el state en cero
    - eliminando las clases de css que ya movimos al otro archivo

--- src/presentation/screens/CounterScreen.tsx
+++ src/presentation/screens/CounterScreen.tsx
@@ -1,26 +1,18 @@
-/* eslint-disable react-native/no-inline-styles */
 import React, { useState } from 'react';
-import { View,Text,StyleSheet,Pressable,Platform } from 'react-native';
-
+import { View,Text,StyleSheet } from 'react-native';
+import { PrimaryButton } from '../../components';
 
 export const CounterScreen = () => {
     const [counter, setcounter] = useState(0);
-
-    // const handleIncrement = ()=>{
-    //   setcounter(prevState => prevState + 1);
-    // };
   return (
     <View style={styles.container}>
         <Text style={styles.title}>{counter}</Text>
 
-        <Pressable
-            style={({pressed})=>[
-                styles.button,
-                pressed && styles.buttonPressed,
-            ]}
-            onPress={() => setcounter(prevState=> prevState + 1)}>
-            <Text style={{color:Platform.OS === 'android' ? 'white' : '#4746AB'}}>+1</Text>
-        </Pressable>
+        <PrimaryButton
+            label="Incrementar"
+            onPress={() => setcounter(prevState=> prevState + 1)}
+            onLongPress={()=> setcounter(0)}
+        />
     </View>
   );
 };
@@ -35,14 +27,5 @@ const styles = StyleSheet.create({
         fontSize:80,
         color:'black',
         fontWeight:800,
-    },
-    button:{
-        backgroundColor: Platform.OS === 'android' ? '#5856D6' : 'white',
-        paddingHorizontal:20,
-        paddingVertical:10,
-        borderRadius:10,
-    },
-    buttonPressed:{
-        backgroundColor: Platform.OS === 'android' ? '#4746AB' : 'white',
     },
 });
