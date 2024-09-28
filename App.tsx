0817bd62c03f61e5a56919a3f3ff394ce2f03dee
Rinel <rineliniguezsosa@gmail.com>
2024-09-27 18:35:08 -0600

    [FEAT] Cambios en el archivo App.tsx
    - comentando el componente CounterScreen
    - importando el componente CounterScreenCopy y devolviendolo en el retorno del componente App

--- App.tsx
+++ App.tsx
@@ -2,14 +2,16 @@
 import React from 'react';
 import { SafeAreaView } from 'react-native';
 // import { HomeScreen } from './src/presentation/screens/HomeScreen';
-import { CounterScreen } from './src/presentation/screens/CounterScreen';
+// import { CounterScreen } from './src/presentation/screens/CounterScreen';
 import { PaperProvider } from 'react-native-paper';
+import { CounterScreenCopy } from './src/presentation/screens/CounterScreenCopy';
+
 
 export const App = () => {
   return (
     <SafeAreaView style={{flex:1}}>
       <PaperProvider>
-        <CounterScreen/>
+        <CounterScreenCopy/>
       </PaperProvider>
     </SafeAreaView>
   );
