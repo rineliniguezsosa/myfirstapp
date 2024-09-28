44da22ebe3ef4cbb66628129adfcfb02a8d6ee87
Rinel <rineliniguezsosa@gmail.com>
2024-09-27 18:16:13 -0600

    [FEAT] Cambios en el archivo App.tsx
    - importando el PaperProvider
    - envolviendo el componente PaperProvider por el componente CounterScreen

--- App.tsx
+++ App.tsx
@@ -3,11 +3,14 @@ import React from 'react';
 import { SafeAreaView } from 'react-native';
 // import { HomeScreen } from './src/presentation/screens/HomeScreen';
 import { CounterScreen } from './src/presentation/screens/CounterScreen';
+import { PaperProvider } from 'react-native-paper';
 
 export const App = () => {
   return (
     <SafeAreaView style={{flex:1}}>
-      <CounterScreen/>
+      <PaperProvider>
+        <CounterScreen/>
+      </PaperProvider>
     </SafeAreaView>
   );
 };
